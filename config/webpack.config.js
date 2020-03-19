1a1ccc5d5b77e79cadf728bdf71127db65fca793
王科迪 <wangkedi.cody@bytedance.com>
2020-03-19 20:03:00 +0800

    error reproduce here

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -68,8 +68,8 @@ module.exports = function(webpackEnv) {
   // common function to get style loaders
   const getStyleLoaders = (cssOptions, preProcessor) => {
     const loaders = [
-      require.resolve('style-loader'),
-      false && {
+      isEnvDevelopment && require.resolve('style-loader'),
+      isEnvProduction && {
         loader: MiniCssExtractPlugin.loader,
         // css is located in `static/css`, use '../../' to locate index.html folder
         // in production `paths.publicUrlOrPath` can be a relative path
